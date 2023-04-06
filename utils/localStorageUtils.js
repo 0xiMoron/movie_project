e465624ed8c59a94f9cc4673084bb9fe426f0c1a
OxiMoron <harrisonkhancock@gmail.com>
2023-04-06 20:03:26 +0900

    changed utils file name added array functions to avoid json parse

--- utils/localStorageUtils.js
+++ utils/localStorageUtils.js
@@ -7,8 +7,21 @@ export function setAndReturnLocalStorageVariable(key, value) {
   return localStorage.getItem(key);
 }
 
+export function setLocalStorageArray(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function setAndReturnLocalStorageArray(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+  return JSON.parse(localStorage.getItem(key));
+}
+
 export function getLocalStorageVariable(key) {
   return localStorage.getItem(key);
+}
+
+export function getLocalStorageArray(key) {
+  return JSON.parse(localStorage.getItem(key));
 }
 
 export function deleteLocalStorageVariable(key) {
