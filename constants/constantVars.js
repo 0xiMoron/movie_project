47d7292fa8622ce6c623720e24e7df07831a7e04
OxiMoron <harrisonkhancock@gmail.com>
2023-04-06 20:00:04 +0900

    Added constant variables for local storage

---
+++ constants/constantVars.js
@@ -0,0 +1,5 @@
+export const SearchedMoviesStorage = "searchedMovies";
+export const SearchedMovieTitleStorage = "searchedMovieTitle";
+export const WatchListedMoviesStorage = "watchListedMovies";
+export const WatchedMoviesStorage = "watchedMovies";
+export const FavoriteMoviesStorage = "favoriteMovies";
